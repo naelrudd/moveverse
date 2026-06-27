bd2bc60b28851a4fc099a7dbbec138b0796191d6
naelrudd <natanaelrudyhadinata@gmail.com>
2026-06-27 19:11:36 +0700

    modified to check bugs

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,15 +2,16 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <SignIn
-        appearance={{
-          variables: {
-            colorBackground: "#ffffff",
-            colorText: "#171717",
-          },
-        }}
-      />
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "white", // force white dulu
+      }}
+    >
+      <SignIn />
     </div>
   );
 }
